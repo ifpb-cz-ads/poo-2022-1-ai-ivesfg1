8f97b53237da5d1ac41651dd6d36d3a36fa1b085
ivesfg1 <wigzfurtado@gmail.com>
2022-05-07 12:33:27 -0300

    inicio do metodo main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import JogadorPackage.Jogador;
+import JogadorPackage.SexoEnum;
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        SexoEnum sexo = null;
+        String nome;
+
+        System.out.println("Bem vindo ao mundo Pokemon!, você é menino ou menina?");
+        System.out.println("\n[0] Menino\n[1] Menina");
+        System.out.print("\nSua opção: ");
+
+        int opcao = input.nextInt();
+
+        if (opcao == 0) {
+            sexo = SexoEnum.MENINO;
+        }
+        else if (opcao == 1) {
+            sexo = SexoEnum.MENINA;
+        }
+
+        System.out.print("Digite seu nome: ");
+        nome = input.nextLine();
+
+        Jogador jogador = new Jogador(nome, sexo);
     }
 }
