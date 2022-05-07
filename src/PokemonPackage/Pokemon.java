4223b46eabbb41777b90b9a95a7be75f35d191c6
ivesfg1 <wigzfurtado@gmail.com>
2022-05-07 10:47:23 -0300

    enviando primeiras versoes dos metodos de ataque e receber dano da classe Pokemon

--- src/PokemonPackage/Pokemon.java
+++ src/PokemonPackage/Pokemon.java
@@ -6,22 +6,34 @@ public class Pokemon {
     private int hp;
     private int ataque;
     private int defesa;
-    private int ataqueEspecial;
-    private int defesaEspecial;
     private int velocidade;
+    private boolean desmaiado;
 
-    public Pokemon(String nome, int hp, int ataque, int defesa, int ataqueEspecial, int defesaEspecial, int velocidade) {
+    public Pokemon(String nome, int hp, int ataque, int defesa, int velocidade) {
         this.nome = nome;
         this.hp = hp;
         this.ataque = ataque;
         this.defesa = defesa;
-        this.ataqueEspecial = ataqueEspecial;
-        this.defesaEspecial = defesaEspecial;
         this.velocidade = velocidade;
+        this.desmaiado = false;
     }
 
     public void atacar(Pokemon pokemonAdversario){
-        pokemonAdversario.sofrerAtaque(this.ataque);
+
+        // TODO: procurar refatorar utilizando metodo abs da lib Math.
+
+        int valorDaDiferenca;
+        int danoARetirar = this.ataque;
+
+        if (pokemonAdversario.getDefesa() > danoARetirar){
+            valorDaDiferenca = pokemonAdversario.getDefesa() - danoARetirar;
+            danoARetirar -= valorDaDiferenca;
+        }
+        else if (pokemonAdversario.getDefesa() < danoARetirar){
+            valorDaDiferenca = danoARetirar - pokemonAdversario.getDefesa();
+            danoARetirar += valorDaDiferenca;
+        }
+        pokemonAdversario.sofrerAtaque(danoARetirar);
     }
 
     public void sofrerAtaque(int valorDoDano){
@@ -42,14 +54,6 @@ public class Pokemon {
 
     public int getDefesa() {
         return defesa;
-    }
-
-    public int getAtaqueEspecial() {
-        return ataqueEspecial;
-    }
-
-    public int getDefesaEspecial() {
-        return defesaEspecial;
     }
 
     public int getVelocidade() {
