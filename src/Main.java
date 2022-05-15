4d82ee013059b9e3cb5789d9747612ca823c929d
ivesfg1 <wigzfurtado@gmail.com>
2022-05-14 22:22:51 -0300

    adicionado requisitos da atividade individual 3.02

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        int variavelInteira = 10;
+        int variavelInteira2 = 5;
 
-        SexoEnum sexo = null;
-        String nome;
+        int somaDasVariaveisInteiras = variavelInteira + variavelInteira2;
 
-        System.out.println("Bem vindo ao mundo Pokemon!, você é menino ou menina?");
-        System.out.println("\n[0] Menino\n[1] Menina");
-        System.out.print("\nSua opção: ");
+        System.out.println(somaDasVariaveisInteiras);
 
-        int opcao = input.nextInt();
+        boolean variavelBooleana = somaDasVariaveisInteiras > 10;
+        System.out.println(variavelBooleana);
 
-        if (opcao == 0) {
-            sexo = SexoEnum.MENINO;
-        }
-        else if (opcao == 1) {
-            sexo = SexoEnum.MENINA;
-        }
+        double variavelDouble = 2.5;
+        int variavelInteira3 = 4;
 
-        System.out.print("Digite seu nome: ");
-        nome = input.nextLine();
+        int parteInteiraDaOperacao = (int) variavelDouble + variavelInteira3;
+        System.out.println(parteInteiraDaOperacao);
 
-        Jogador jogador = new Jogador(nome, sexo);
+        // como o intuito da atividade atual é apenas declarar algumas variaveis e fazer operações logicas
+        // aproveitei o fato de ainda nao ter feito codigo suficiente pra iniciar o metodo main e decidi deixar
+        // comentado abaixo
+
+//        Scanner input = new Scanner(System.in);
+//
+//        SexoEnum sexo = null;
+//        String nome;
+//
+//        System.out.println("Bem vindo ao mundo Pokemon!, você é menino ou menina?");
+//        System.out.println("\n[0] Menino\n[1] Menina");
+//        System.out.print("\nSua opção: ");
+//
+//        int opcao = input.nextInt();
+//
+//        if (opcao == 0) {
+//            sexo = SexoEnum.MENINO;
+//        }
+//        else if (opcao == 1) {
+//            sexo = SexoEnum.MENINA;
+//        }
+//
+//        System.out.print("Digite seu nome: ");
+//        nome = input.nextLine();
+//
+//        Jogador jogador = new Jogador(nome, sexo);
     }
 }
