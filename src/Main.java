105577e15c48371880abf05e6e235a426a61c6bc
ivesfg1 <wigzfurtado@gmail.com>
2022-06-04 22:37:31 -0300

    adicionado while conforme atividade sugere

--- src/Main.java
+++ src/Main.java
@@ -5,60 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // mais uma vez, ainda nao possui codigo suficiente desenvolvido para que as variaveis abaixo sejam condizentes
-        // com a ideia do meu projeto.
+        while (true) { // acredito que o while que engloba o projeto (pra que ele funcione infinitamente ja baste)
+            Scanner input = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
+            SexoEnum sexo = null;
+            String nome;
 
-        System.out.print("Digite um numero: ");
-        int primeiroNumero = input.nextInt();
+            System.out.println("Bem vindo ao mundo Pokemon!, você é menino ou menina?");
+            System.out.println("\n[0] Menino\n[1] Menina");
+            System.out.print("\nSua opção: ");
 
-        System.out.print("Digite outro numero: ");
-        int segundoNumero = input.nextInt();
+            int opcao = input.nextInt();
 
-        int soma = primeiroNumero + segundoNumero;
-        System.out.println("A soma dos numeros digitados eh igual a: " + soma);
+            if (opcao == 0) {
+                sexo = SexoEnum.MENINO;
+            }
+            else if (opcao == 1) {
+                sexo = SexoEnum.MENINA;
+            }
 
-        if (soma > 10){
-            System.out.println("A soma dos numeros informados eh maior do que 10");
-        } else {
-            System.out.println("A soma dos numeros informados nao eh maior do que 10");
+            System.out.print("Digite seu nome: ");
+            nome = input.nextLine();
+
+            Jogador jogador = new Jogador(nome, sexo);
         }
-
-        switch (soma % 2){
-            case 0:
-                System.out.println("A soma dos numeros informados resulta em um numero par.");
-                break;
-            default:
-                System.out.println("A soma dos numeros informados resulta em um numero impar.");
-                break;
-        }
-
-        // como o intuito da atividade atual é apenas declarar algumas variaveis e fazer operações logicas alem de
-        // utilizar desvio condicional (if-else ou switch-case aproveitei o fato de ainda nao ter feito codigo suficiente
-        // pra iniciar o metodo main e decidi deixar comentado abaixo
-
-//        Scanner input = new Scanner(System.in);
-//
-//        SexoEnum sexo = null;
-//        String nome;
-//
-//        System.out.println("Bem vindo ao mundo Pokemon!, você é menino ou menina?");
-//        System.out.println("\n[0] Menino\n[1] Menina");
-//        System.out.print("\nSua opção: ");
-//
-//        int opcao = input.nextInt();
-//
-//        if (opcao == 0) {
-//            sexo = SexoEnum.MENINO;
-//        }
-//        else if (opcao == 1) {
-//            sexo = SexoEnum.MENINA;
-//        }
-//
-//        System.out.print("Digite seu nome: ");
-//        nome = input.nextLine();
-//
-//        Jogador jogador = new Jogador(nome, sexo);
     }
 }
